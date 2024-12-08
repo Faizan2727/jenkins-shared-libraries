00021b8a5aa8337844183927400eed45b00df7e9
FAIZAN SHAIKH <faizanshaikhbmw2727@gmail.com>
2024-12-08 16:30:01 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,3 +1,3 @@
-def call(String ProjectName, String ImageTag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+def call(String appName, String tag) {
+    sh "docker build -t ${appName}:${tag} ."
 }
